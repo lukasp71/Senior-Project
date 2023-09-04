457508ded51d5df09ca5050aede2fb9a74a3e7c5
edwardn117 <125089258+edwardn117@users.noreply.github.com>
2023-09-04 12:46:53 -0400

    Test with home screen

--- senior_project/lib/screens/home/home.dart
+++ senior_project/lib/screens/home/home.dart
@@ -2,28 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:senior_project/services/auth.dart';
+final AuthService _auth = AuthService();
 
-class Home extends StatelessWidget {
-  final AuthService _auth = AuthService();
+class Home extends StatefulWidget {
+  const Home({super.key});
+   
+@override
+_HomeScreenState createState() => _HomeScreenState();
 
+ 
+}
+class _HomeScreenState extends State <Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
+      backgroundColor: Colors.green[50],
+       body: Stack(children: [
           TextButton.icon(
             icon: Icon(Icons.person),
             onPressed: () async {
               await _auth.signOut();
             },
             label: Text('logout'),
-          )
-        ],
-      ),
+          ),
+           Positioned(
+              top: 0,
+              left: 30,
+              child: Text('Hello',
+                style: TextStyle(
+                  color: Colors.black12,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 10,
+                )
+              )
+           )
+       ]
+       )
     );
   }
 }
