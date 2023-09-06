ca82f76f42460ffbe68125a5d23e83e6bf6202f4
lukasp71 <lukeperri71@gmail.com>
2023-09-06 14:21:19 -0400

    get started page

---
+++ senior_project/lib/screens/home/get_started.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import 'package:senior_project/main.dart';
+
+class getStartedPage extends StatelessWidget {
+  const getStartedPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MyApp();
+  }
+}
