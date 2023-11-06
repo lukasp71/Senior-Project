fac28d4854b0161af5d6e34defa92880b68bd775
lukasp71 <lukeperri71@gmail.com>
2023-11-06 11:59:19 -0500

    home page update

--- senior_project/lib/news_section/screens/home_page.dart
+++ senior_project/lib/news_section/screens/home_page.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:senior_project/news_section/constants/constants.dart';
 import 'package:senior_project/news_section/controllers/news_controller.dart';
-import 'package:senior_project/news_section/screens/get_started.dart';
 import 'package:senior_project/news_section/widgets/custom_appBar.dart';
-import 'package:senior_project/news_section/widgets/news_card.dart';
 import 'package:senior_project/news_section/widgets/side_drawer.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -15,41 +13,15 @@ class HomePage extends StatelessWidget {
   NewsController newsController = Get.put(NewsController());
 
   // Function to show the WelcomePage as a popup
-  Future<void> showWelcomePopup(BuildContext context) async {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-          child: WelcomePage(),
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
-    // Show the welcome popup when the HomePage first loads up
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
-      showWelcomePopup(context);
-    });
-
     return Scaffold(
-      drawer: sideDrawer(newsController as BuildContext),
-      appBar: customAppBar('Threat Detection Hub', context, actions: [
-        IconButton(
-          onPressed: () {
-            newsController.category.value = '';
-            newsController.searchNews.value = '';
-            newsController.channel.value = '';
-            newsController.cName.value = '';
-            newsController.update();
-          },
-          icon: const Icon(Icons.refresh),
-        ),
-      ]),
+      drawer: sideDrawer(context, newsController),
+      appBar: customAppBar('Threat Detection Hub', context, actions: []),
       body: Obx(() {
         if (newsController.isLoading.value && newsController.allNews.isEmpty) {
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         } else {
           return SingleChildScrollView(
             keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
@@ -191,7 +163,6 @@ class HomePage extends StatelessWidget {
                         : controller.allNews.isEmpty
                             ? const Center(child: CircularProgressIndicator())
                             : ListView.builder(
-                                controller: controller.scrollController,
                                 physics: const NeverScrollableScrollPhysics(),
                                 shrinkWrap: true,
                                 itemCount: controller.allNews.length,
@@ -211,19 +182,6 @@ class HomePage extends StatelessWidget {
                                         print('Could not launch $articleUrl');
                                       }
                                     },
-                                    child: NewsCard(
-                                      imgUrl: controller
-                                              .allNews[index].urlToImage ??
-                                          '',
-                                      desc: controller
-                                              .allNews[index].description ??
-                                          '',
-                                      title: controller.allNews[index].title,
-                                      content:
-                                          controller.allNews[index].content ??
-                                              '',
-                                      postUrl: controller.allNews[index].url,
-                                    ),
                                   );
                                 },
                               );
