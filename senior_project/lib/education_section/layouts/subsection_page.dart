b0a0afc5cff478add53490ae776cec7bcfdfe0fd
lukasp71 <lukeperri71@gmail.com>
2023-11-06 11:55:45 -0500

    bug fix

--- senior_project/lib/education_section/layouts/subsection_page.dart
+++ senior_project/lib/education_section/layouts/subsection_page.dart
@@ -1,76 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:senior_project/education_section/layouts/Module.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:senior_project/education_section/layouts/educational_module_sidebar.dart';
 
 class SubsectionPage extends StatelessWidget {
   final String subsectionTitle;
   final String content;
-  final Module module;
-  final int subsectionIndex;
+  final VoidCallback onForward;
+  final VoidCallback onBackward;
 
-  SubsectionPage(
-      this.subsectionTitle, this.content, this.module, this.subsectionIndex);
-//test
+  SubsectionPage({
+    required this.subsectionTitle,
+    required this.content,
+    required this.onForward,
+    required this.onBackward,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(subsectionTitle),
+        title: Text(
+          subsectionTitle,
+          style: GoogleFonts.montserrat(),
+        ),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
-            Text(content),
+            Text(
+              content,
+              style: GoogleFonts.montserrat(
+                fontSize: 18.0,
+              ),
+            ),
+            SizedBox(
+                height: 20.0), // Add spacing between text and the next widget
+            SizedBox(
+                height: 20.0), // Add spacing between button and the next widget
             // ... other UI elements for the subsection page
           ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: subsectionIndex > 0
-                  ? () {
-                      // Navigate to the previous subsection page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SubsectionPage(
-                            module.subsections[subsectionIndex - 1],
-                            'Content for ' +
-                                module.subsections[subsectionIndex - 1],
-                            module,
-                            subsectionIndex - 1,
-                          ),
-                        ),
-                      );
-                    }
-                  : null, // Disable the button if it's the first subsection
-            ),
-            IconButton(
-              icon: Icon(Icons.arrow_forward),
-              onPressed: subsectionIndex < module.subsections.length - 1
-                  ? () {
-                      // Navigate to the next subsection page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SubsectionPage(
-                            module.subsections[subsectionIndex + 1],
-                            'Content for ' +
-                                module.subsections[subsectionIndex + 1],
-                            module,
-                            subsectionIndex + 1,
-                          ),
-                        ),
-                      );
-                    }
-                  : null, // Disable the button if it's the last subsection
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: onBackward,
+                iconSize: 30.0,
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: onForward,
+                iconSize: 30.0,
+              ),
+            ],
+          ),
         ),
       ),
       drawer: SidebarMenu(),
